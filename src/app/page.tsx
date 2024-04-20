30e941374567bcd594a1fef4a576dbff25562c04
Eren-Yeaager <saswatmarpureddy@gmail.com>
2024-04-20 20:32:39 +0530

    card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from "@/components/toggle";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card1 } from "@/components/Card";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -15,10 +16,18 @@ export default function Home() {
           <ModeToggle />
         </div>
         <div className="flex space-x-4 justify-center">
-          <div className="cursor-pointer">Home</div>
-          <div className="cursor-pointer">About</div>
+          <div className="button rounded-full px-4 py-2 transition duration-300 hover:bg-gray-600 hover:shadow-lg cursor-pointer">
+            Home
+          </div>
+          <div className="button rounded-full px-4 py-2 transition duration-300 hover:bg-gray-600 hover:shadow-lg cursor-pointer">
+            About
+          </div>
         </div>
         <Button>Connect Wallet</Button>
+      </div>
+
+      <div className="card-container flex justify-center mt-4">
+        <Card1 />
       </div>
     </main>
   );
---
+++ src/components/Card.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export function Card1() {
+  return (
+    <Card className="w-[700px]">
+      <CardHeader>
+        <CardTitle>Create memory</CardTitle>
+        <CardDescription>
+          Deploy your new memory to IPFS in one-click.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your photo" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="emotion">Emotion</Label>
+              <Select>
+                <SelectTrigger id="emotion">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="happy">Happy</SelectItem>
+                  <SelectItem value="sad">Sad</SelectItem>
+                  <SelectItem value="angry">Angry</SelectItem>
+                  <SelectItem value="meh">Meh!</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  );
+}
